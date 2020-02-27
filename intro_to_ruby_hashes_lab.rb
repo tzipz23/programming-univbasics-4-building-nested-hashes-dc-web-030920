ea572c6af5289ee737b9dcc24307f1433ca1f093
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-27 02:59:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one_piece_owned =>
+        :one_piece_owned => 25,
+        :two_pieces_owned =>
       }
     }
   }
